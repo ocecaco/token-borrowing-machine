512861e1e274bca8fe5b21e096125fcdf480a9c4
Daniël Louwrink <daniel.louwrink@gmail.com>
2021-07-02 11:57:35 +0200

    update exclusive machine

--- src/machine.rs
+++ src/machine.rs
@@ -3,15 +3,21 @@ use std::collections::HashMap;
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum RefState {
     Created,
-    TokenBorrowedFrom(Reference),
+    Borrowing,
     Dead,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct RefInfo {
+    parent: Reference,
+    state: RefState,
 }
 
 #[derive(Debug, Clone)]
 pub struct TokenMachine {
     ref_count: u32,
     current_owner: Reference,
-    ref_state: HashMap<Reference, RefState>,
+    ref_info: HashMap<Reference, RefInfo>,
 }
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
@@ -20,52 +26,63 @@ pub struct Reference(u32);
 impl TokenMachine {
     pub fn init() -> (Reference, Self) {
         let initial_ref = Reference(0);
-        let mut initial_state = HashMap::new();
-        initial_state.insert(initial_ref, RefState::TokenBorrowedFrom(initial_ref));
+        let mut ref_info = HashMap::new();
+        ref_info.insert(
+            initial_ref,
+            RefInfo {
+                // For simplicity, the initial reference has itself as its parent.
+                // This means we don't require an Option to distinguish whether a
+                // reference has a parent or not.
+                parent: initial_ref,
+                state: RefState::Borrowing,
+            },
+        );
+
         (
             initial_ref,
             TokenMachine {
-                ref_count: initial_ref.0 + 1,
+                ref_count: 1,
                 current_owner: initial_ref,
-                ref_state: initial_state,
+                ref_info,
             },
         )
     }
 
-    pub fn create_ref(&mut self) -> Reference {
+    pub fn create_ref(&mut self, parent: Reference) -> Reference {
         let id = self.ref_count;
         self.ref_count += 1;
-
         let new_ref = Reference(id);
-        self.ref_state.insert(new_ref, RefState::Created);
+
+        self.ref_info.insert(
+            new_ref,
+            RefInfo {
+                state: RefState::Created,
+                parent,
+            },
+        );
 
         new_ref
     }
 
-    pub fn lend_token(&mut self, target: Reference) {
-        let target_state = self.ref_state[&target];
-        match target_state {
+    pub fn borrow_token(&mut self, target: Reference) {
+        let target_info = self.ref_info[&target];
+        match target_info.state {
             RefState::Created => {}
-            RefState::TokenBorrowedFrom { .. } => panic!("Cannot create borrowing cycle"),
+            RefState::Borrowing => panic!("Cannot create borrowing cycle"),
             RefState::Dead => panic!("Target cannot be dead"),
         };
 
-        self.ref_state
-            .insert(target, RefState::TokenBorrowedFrom(self.current_owner));
+        self.ref_info.get_mut(&target).unwrap().state = RefState::Borrowing;
         self.current_owner = target;
     }
 
     pub fn return_token(&mut self) {
-        let current_owner = self.current_owner;
-        let original_owner =
-            if let RefState::TokenBorrowedFrom(source) = self.ref_state[&current_owner] {
-                source
-            } else {
-                panic!("Invariant violation: reference has token without borrowing it from anyone")
-            };
+        let current = self.current_owner;
+        let current_info = self.ref_info[&current];
+        let parent = current_info.parent;
 
-        self.ref_state.insert(current_owner, RefState::Dead);
-        self.current_owner = original_owner;
+        self.ref_info.get_mut(&current).unwrap().state = RefState::Dead;
+        self.current_owner = parent;
     }
 
     pub fn use_token(&mut self, source: Reference) {
